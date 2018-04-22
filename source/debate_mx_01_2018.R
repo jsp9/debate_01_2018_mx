7265014bff9b63e5c0f09726ceb98267fdea742d
Jose Antonio Sanchez <pepe_opi@Joses-MacBook-Air.local>
2018-04-21 20:20:20 -0500

    Actualiación

--- source/debate_mx_01_2018.R
+++ source/debate_mx_01_2018.R
@@ -6,6 +6,7 @@ tokens<-read.csv('./source/tokens.csv', stringsAsFactors = F)
 
 library(rtweet)
 library(data.table)
+library(ggplot2)
 
 Sys.setenv(TZ='America/Mexico_City')
 token_1<-create_token(app = tokens$app[1],
@@ -16,11 +17,18 @@ token_1<-create_token(app = tokens$app[1],
 data<-NULL
 data_cons<-data.table(hora=.POSIXct(Sys.time(), 'America/Mexico_City'), n_tweets=NA, n_amlo=NA,
                       n_meade=NA, n_anaya=NA, n_bronco=NA, n_maza=NA)
+data_cons_nort<-data.table(hora=.POSIXct(Sys.time(), 'America/Mexico_City'), n_tweets=NA, n_amlo=NA,
+                      n_meade=NA, n_anaya=NA, n_bronco=NA, n_maza=NA)
+
 data_i<-data.table(hora=NA, n_tweets=NA, n_amlo=NA,
                       n_meade=NA, n_anaya=NA, n_bronco=NA, n_maza=NA)
-horas<-4
+data_i_nort<-data.table(hora=NA, n_tweets=NA, n_amlo=NA,
+                   n_meade=NA, n_anaya=NA, n_bronco=NA, n_maza=NA)
 
-for(i in 1:(60*horas)){
+
+horas<-4
+#1:(60*horas)
+for(i in 1:5){
   if(i %% 60==0) {
   message(paste('Ya son las', Sys.time()))
   }
@@ -28,22 +36,35 @@ data<-stream_tweets(
   "@lopezobrador_, @JoseAMeadeK, @JaimeRdzNL, @RicardoAnayaC, @Mzavalagc",
   timeout = 60,
   file_name = 'PRUEBA.json',
-  parse = TRUE, verbose = F)
+  parse = TRUE, verbose = T)
 write_as_csv(data, paste0('./data/tw_', i,'.csv'))
 #####RESUMEN#####
 data_i$hora<-.POSIXct(Sys.time(), 'America/Mexico_City')
 data_i$n_tweets<-nrow(data)
-data_i$n_amlo<-sum(grepl('lopezobrador_', data$text))
-data_i$n_meade<-sum(grepl('JoseAMeadeK', data$text))
-data_i$n_anaya<-sum(grepl('RicardoAnayaC', data$text))
-data_i$n_maza<-sum(grepl('Mzavalagc', data$text))
-data_i$n_bronco<-sum(grepl('JaimeRdzNL', data$text))
+data_i$n_amlo<-sum(unlist(lapply(data$mentions_screen_name, function(x) sum(grepl('lopezobrador_', x)))))
+data_i$n_meade<-sum(unlist(lapply(data$mentions_screen_name, function(x) sum(grepl('JoseAMeadeK', x)))))
+data_i$n_anaya<-sum(unlist(lapply(data$mentions_screen_name, function(x) sum(grepl('RicardoAnayaC', x)))))
+data_i$n_maza<-sum(unlist(lapply(data$mentions_screen_name, function(x) sum(grepl('Mzavalagc', x)))))
+data_i$n_bronco<-sum(unlist(lapply(data$mentions_screen_name, function(x) sum(grepl("JaimeRdzNL", x)))))
 data_cons<-rbind(data_cons, data_i)
 
+#####NO RTS#####
+data_nort<-data[data$is_retweet==F,]
 
-##plotea menciones caa 5 minutos##
+data_i_nort$hora<-.POSIXct(Sys.time(), 'America/Mexico_City')
+data_i_nort$n_tweets<-nrow(data_nort)
+data_i_nort$n_amlo<-sum(unlist(lapply(data_nort$mentions_screen_name, function(x) sum(grepl('lopezobrador_', x)))))
+data_i_nort$n_meade<-sum(unlist(lapply(data_nort$mentions_screen_name, function(x) sum(grepl('JoseAMeadeK', x)))))
+data_i_nort$n_anaya<-sum(unlist(lapply(data_nort$mentions_screen_name, function(x) sum(grepl('RicardoAnayaC', x)))))
+data_i_nort$n_maza<-sum(unlist(lapply(data_nort$mentions_screen_name, function(x) sum(grepl('Mzavalagc', x)))))
+data_i_nort$n_bronco<-sum(unlist(lapply(data_nort$mentions_screen_name, function(x) sum(grepl("JaimeRdzNL", x)))))
+data_cons_nort<-rbind(data_cons_nort, data_i_nort)
+
+#############plotea menciones cadaa 5 minutos#########
+######Grafica con rt########
 if (i %%5 ==0){
-  message('actuaizando gráficas...')
+  message('actuaizando gráficas de 5 minutos...')
+  
 plot_menciones<-ggplot(data=data_cons[-1,], aes(x=hora, y=n_tweets))+
   geom_line(color='grey')+
   geom_line(aes(x=hora, y=n_amlo), color='#AC2C2C')+
@@ -54,11 +75,87 @@ plot_menciones<-ggplot(data=data_cons[-1,], aes(x=hora, y=n_tweets))+
   theme_light()+
   ylab('Número de menciones')+
   xlab('Hora')+
-  ggtitle('Número de menciones por minuto', subtitle = 'Gris:Número de menciones total \n Marrón:@lopezobrador_ \n Rojo:@JoseAMeadeK \n Azul:@RicardoAnayaC \n Violeta:@Mzavalagc \n Azul marino:@JaimeRdzNL')
+  ggtitle('Número de menciones por minuto',
+          subtitle = paste0('Gris:Número de menciones total \n',
+                            'Marrón:@lopezobrador_ \n',
+                            'Rojo:@JoseAMeadeK \n',
+                            'Azul:@RicardoAnayaC \n',
+                            'Violeta:@Mzavalagc \n',
+                            'Azul marino:@JaimeRdzNL \n',
+                            'Nota: Las menciones no son excluyentes entre candidatos.\n',
+                            'También se consideran tweets que citan menciones a los candidatos para el total de tweets'))
 ggsave(path = getwd(),device = 'png',plot =  plot_menciones,filename = 'plot_menciones.png')
 #system(paste0('open ', getwd(), '/plot_menciones.png'))
-system('git add .')
-system("git commit -m 'Actualiación' ")
-system("git push origin master")
+
+
+######Grafica SIN rt########
+
+plot_menciones_nort<-ggplot(data=data_cons_nort[-1,], aes(x=hora, y=n_tweets))+
+  geom_line(color='grey')+
+  geom_line(aes(x=hora, y=n_amlo), color='#AC2C2C')+
+  geom_line(aes(x=hora, y=n_meade), color='red')+
+  geom_line(aes(x=hora, y=n_anaya), color='blue')+
+  geom_line(aes(x=hora, y=n_bronco), color='#2D3672')+
+  geom_line(aes(x=hora, y=n_maza), color='#B917BF')+
+  theme_light()+
+  ylab('Número de menciones')+
+  xlab('Hora')+
+  ggtitle('Número de menciones por minuto (menos RTS)',
+          subtitle = paste0('Gris:Número de menciones sin Retweets \n',
+                            'Marrón:@lopezobrador_ \n',
+                            'Rojo:@JoseAMeadeK \n',
+                            'Azul:@RicardoAnayaC \n',
+                            'Violeta:@Mzavalagc \n',
+                            'Azul marino:@JaimeRdzNL \n',
+                            'Nota: Las menciones no son excluyentes entre candidatos.\n',
+                            'También se consideran tweets que citan menciones a los candidatos para el total de tweets'))
+ggsave(path = getwd(),device = 'png',plot =  plot_menciones_nort,filename = 'plot_menciones_nort.png')
+#system(paste0('open ', getwd(), '/plot_menciones.png'))
+
+
+data_cons_cumsum<-cbind.data.frame(hora=data_cons_nort$hora[-1],
+                                   apply(data_cons_nort[-1, c( 'n_amlo', 'n_meade',
+                                                          'n_anaya', 'n_bronco', 'n_maza')], 2,  cumsum ))
+
+data_cons_cumsum[, c('n_amlo',
+                     'n_meade',
+                     'n_anaya',
+                     'n_bronco',
+                     'n_maza')]<-round(data_cons_cumsum[, c('n_amlo',
+                                                      'n_meade',
+                                                      'n_anaya',
+                                                      'n_bronco',
+                                                      'n_maza')]/rowSums(data_cons_cumsum[, c('n_amlo',
+                                                                                              'n_meade','n_anaya',
+                                                                                              'n_bronco',
+                                                                                              'n_maza')])*100,1)
+
+######Grafica PORC MECIONES ACUMULADA########
+
+plot_menciones_nort_porc<-ggplot(data=data_cons_cumsum, aes(x=hora, y=n_amlo))+
+  geom_line(color='#AC2C2C')+
+ # geom_line(aes(x=hora, y=n_amlo), color='#AC2C2C')+
+  geom_line(aes(x=hora, y=n_meade), color='red')+
+  geom_line(aes(x=hora, y=n_anaya), color='blue')+
+  geom_line(aes(x=hora, y=n_bronco), color='#2D3672')+
+  geom_line(aes(x=hora, y=n_maza), color='#B917BF')+
+  theme_light()+
+  ylab('% acumulado de menciones')+
+  xlab('Hora')+
+  ylim(c(0, 100))+
+  ggtitle('Procentaje acumulado de menciones sin Retweets',
+          subtitle = paste0('Marrón:@lopezobrador_ \n',
+                            'Rojo:@JoseAMeadeK \n',
+                            'Azul:@RicardoAnayaC \n',
+                            'Violeta:@Mzavalagc \n',
+                            'Azul marino:@JaimeRdzNL \n'))
+ggsave(path = getwd(),
+       device = 'png',
+       plot =  plot_menciones_nort_porc,
+       filename = 'plot_menciones_nort_porc.png')
+
+#system('git add .')
+#system("git commit -m 'Actualiación' ")
+#system("git push origin master")
 }
 }
